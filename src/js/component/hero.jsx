ef04f6e13ba98bc2b086aedb69b013000a88be0d
Jose Hurtado <hello@hurtadojose.com>
2022-07-15 20:25:53 +0000

    Slider Component

--- src/js/component/hero.jsx
+++ src/js/component/hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Hero1 from "/src/img/hero_1.webp";
 import Hero2 from "/src/img/hero_2.webp";
 import Hero3 from "/src/img/hero_3.webp";
@@ -7,28 +6,40 @@ import Hero3 from "/src/img/hero_3.webp";
 
 export const Hero = () => {
 	return (
-        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
-        <div className="carousel-inner heroImage">
-            <div className="carousel-item active ">
+        <div className="carousel-inner">
+            <div className="carousel-item active">
             <img src={Hero1} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+            </div>
             </div>
             <div className="carousel-item">
             <img src={Hero2} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+            </div>
             </div>
             <div className="carousel-item">
             <img src={Hero3} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
             </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Previous</span>
         </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Next</span>
         </button>
